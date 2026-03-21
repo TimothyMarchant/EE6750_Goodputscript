92f8475fd7cb2215040fbe4dd084f88d61b2952b
Timothy Marchant <tim.marchant@outlook.com>
2026-03-21 11:29:42 -0400

    added verbose and other stuff.

--- RunSims.sh
+++ RunSims.sh
@@ -1,16 +1,28 @@
 # !/bin/bash
+ProgressBar(){
+    Total=$1
+    Current=$2
 
+    printf $2"/"$1"\r"
+}
 NumberOfCarsArray=(2 4 6 8 10 12 14 16 18 20)
+
 ScriptName="real_per_goodput_ring.cc"
 StartSeed=1
-EndSeed=10
-
+EndSeed=2
+declare -i TotalRuns=($EndSeed*${#NumberOfCarsArray[@]})
+CurrentRun=0
 #Run loop through the amount of cars in the car number array
 for i in ${NumberOfCarsArray[@]};
 do
+
     #run over several trials.  You should get similiar results.
     for ((j = $StartSeed ; j <= $EndSeed ; j++));
-    do
-        ./ns3 run scratch/$ScriptName -- --Filename=$i"_Cars_Goodput" --Foldername=$i"_Cars_Output" --seed=$j --numVehicles=$i
+    do 
+        declare -i Current=($CurrentRun*$EndSeed+$j)
+        ProgressBar $TotalRuns $Current
+        ./ns3 run scratch/$ScriptName -- --Filename=$i"_Cars_Goodput" --Foldername=$i"_Cars_Output" --seed=$j --numVehicles=$i --verbose=0
     done
+    CurrentRun=($CurrentRun+1)
 done
+echo "DONE"
