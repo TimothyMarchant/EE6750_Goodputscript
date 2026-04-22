6affc0c65d22bcb9a8e8c99a9d478ec778366148
Timothy Marchant <tim.marchant@outlook.com>
2026-04-22 12:12:15 -0400

    Got correct CSV files now.  Working on making plots now.

--- PythonSrc/PCAPAnalyze.py
+++ PythonSrc/PCAPAnalyze.py
@@ -7,11 +7,11 @@ Experiment2Folder="SpeedExperiment_OutputPCAPS"
 Experiment3Folder="PERExperiment_OutputPCAPS"
 ExperimentFolders=[Experiment1Folder,Experiment2Folder,Experiment3Folder]
 def ReadPCAPs(Path,OutputPath):
-    PCAPFolder="PCAP/"
-    PCAPFiles=os.listdir(Path+"/"+PCAPFolder)
+
+    PCAPFiles=os.listdir(Path)
     arr=np.zeros(shape=(len(PCAPFiles)))
     for i in range (0,len(PCAPFiles)):
-        CAP=pyshark.FileCapture(Path+"/"+PCAPFolder+PCAPFiles[i],display_filter='wlan.fc.retry == True')
+        CAP=pyshark.FileCapture(Path+PCAPFiles[i],display_filter='wlan.fc.retry == True')
         index=0
         for j in CAP:
             index=index+1
@@ -19,10 +19,22 @@ def ReadPCAPs(Path,OutputPath):
         arr[i]=index
         print(i)
     np.savetxt(OutputPath+".csv",arr)
+QUICOutputFolder="QUICMACRETRYS"
+TCPOutputFolder="TCPMACRETRYS"
 
+try:
+    os.mkdir(QUICOutputFolder)
+    os.mkdir(TCPOutputFolder)
+
+except Exception as e:
+    print(e)
 for i in ExperimentFolders:
-    ReadPCAPs(i+"/"+"QUIC",i+"_QUIC_MACRETRIES")
+    QUICSubFolders=os.listdir(i+"/QUIC/PCAP")
+    TCPSubFolders=os.listdir(i+"/TCP/PCAP")
+    for j in QUICSubFolders:
+        ReadPCAPs(i+"/"+"QUIC/PCAP/"+j+"/",QUICOutputFolder+"/"+i+"_"+j+"_QUIC_MACRETRIES")
     print("Finished half of: "+i)
-    ReadPCAPs(i+"/"+"TCP",i+"_TCP_MACRETRIES")
+    for j in TCPSubFolders:
+        ReadPCAPs(i+"/"+"TCP/PCAP/"+j+"/",TCPOutputFolder+"/"+i+"_"+j+"_TCP_MACRETRIES")
     print("Finished: "+i)
 print("Finished getting mac retries")
